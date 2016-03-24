3d38e1148fcd217c2792eb350215c5e32491b44e
Oren Lederman <oren.lederman@gmail.com>
2016-03-24 15:04:43 -0400

    Scan promies wrapper also supports timeout

--- src/nRF_mobile/www/js/bundle.js
+++ src/nRF_mobile/www/js/bundle.js
@@ -80,7 +80,7 @@ var app = {
         deviceList.innerHTML = ''; // empties the list
         app.startScan().then(
         function(obj){ // success
-            console.log("Scan Success - "+obj)
+            console.log("Scan completed successfully - "+obj.status)
         }, function(obj) { // error
             console.log("Scan Start error: " + obj.error + " - " + obj.message)
         }, function(obj) { // progress
@@ -102,31 +102,50 @@ var app = {
     },
     startScan: function() {
         var deferred = Q.defer();
+        var params = {
+          services:[],
+          allowDuplicates: false,
+          scanMode: bluetoothle.SCAN_MODE_LOW_POWER,
+          matchMode: bluetoothle.MATCH_MODE_STICKY,
+          matchNum: bluetoothle.MATCH_NUM_ONE_ADVERTISEMENT,
+          //callbackType: bluetoothle.CALLBACK_TYPE_FIRST_MATCH,
+          scanTimeout: 10000,
+        };
 
-        function startScanSuccess(obj) {
-            if (obj.status == "scanResult") {
-                deferred.notify(obj);    
-            }
-            else if (obj.status == "scanStarted")
-            {
-                //console.log("Scan was started successfully, stopping in 10");
-                //scanTimer = setTimeout(app.scanTimeout, 10000);
-            }
-            else
-            {
-                obj.error = "unhandled state";
-                deferred.reject(obj); // unhandled state
-            }
-            
-        };
-        function startScanError(obj) {
-            deferred.reject(obj);
-        };
+        // setup timeout if requested
+        var scanTimer = null;
+        if (params.scanTimeout) {
+            scanTimer = setTimeout(function() {
+                console.log('Stopping scan');
+                bluetoothle.stopScan(
+                    function(obj) {
+                      deferred.resolve(obj);
+                    },
+                    function(obj) {
+                      deferred.reject(obj);
+                    }
+                );
+            }, params.scanTimeout);
+        }
 
         console.log('Start scan');
         deviceList.innerHTML = ''; // empties the list
 
-        bluetoothle.startScan(startScanSuccess, startScanError, {});
+        bluetoothle.startScan(
+            function startScanSuccess(obj) {
+                if (obj.status == "scanResult") {deferred.notify(obj);}
+                else if (obj.status == "scanStarted"){}// do nothing. it started ok
+                else {
+                    obj.error = "unhandled state";
+                    clearTimeout(scanTimer);
+                    deferred.reject(obj); // unhandled state
+                }
+            }, 
+            function startScanError(obj) {
+                clearTimeout(scanTimer);
+                deferred.reject(obj);
+            }, 
+            params);
 
         return deferred.promise;
     },
