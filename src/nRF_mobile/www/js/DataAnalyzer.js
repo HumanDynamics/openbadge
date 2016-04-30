8f4ab679c1ac9f03d4113763c3e5b95ab853af97
Oren Lederman <oren.lederman@gmail.com>
2016-04-30 13:09:43 -0400

    Generating talking intervals

--- src/nRF_mobile/www/js/DataAnalyzer.js
+++ src/nRF_mobile/www/js/DataAnalyzer.js
@@ -4,100 +4,151 @@ It uses a moving RMS to determine a threshold. Since multiplication is
 easier than a root operation, we calculate MS and not RMS.
 */
 
-function DataAnalyzer() {
-	// if at least this amount of time happens between a null signal
-	// and a talk signal, they are considered to have started talking.
-	var MIN_TALK_LENGTH = 200;
-	// If we get no signal for this amount of time, consider them no
-	// longer talking.
-	var TALK_TIMEOUT = 1000;
-	
-	var samples = []; // array of samples : timestamp, volume
-	var smoother = new SmoothArray(); // array object used for caluclating smooth value
-	var rmsThreshold = new RMSThreshold();
+function DataAnalyzer(sampleFreq) {
+    this.sampleFreq= sampleFreq;
 
-	// Adds a sample. Code assumes that the timestamp is monotonically increasing
-	this.addSample = function(vol,timestamp) {
-		// remove old objects samples array
-		// TODO - implement
+    // if at least this amount of time happens between a null signal
+    // and a talk signal, they are considered to have started talking.
+    var MIN_TALK_LENGTH = 200;
+    // If we get no signal for this amount of time, consider them no
+    // longer talking.
+    var TALK_TIMEOUT = 1000;
 
-		// Calc smoothened volume
-		var sv = smoother.push(vol);
 
-		// update RMS with new sample
-		rmsThreshold.addSample(sv);
+    var samples = []; // array of samples : timestamp, volume
+    var smoother = new SmoothArray(); // array object used for caluclating smooth value
+    var rmsThreshold = new RMSThreshold();
 
-		// speaking indicator is 1 if smoothened volume is above the threshold
-		var isSpeak = rmsThreshold.compareToThreshold(sv);
+    // Adds a sample. Code assumes that the timestamp is monotonically increasing
+    // Timestamp is a Date object
+    this.addSample = function(vol,timestamp) {
+        // remove old objects samples array
+        // TODO - implement
 
-		// add sample to array
-		samples.push({'vol': vol, 'sv' : sv, 'isSpeak' : isSpeak,'timestamp': timestamp});
-	};
+        // Calc smoothened volume
+        var sv = smoother.push(vol);
 
-	this.getSamples = function() {
-		return samples;
-	}
+        // update RMS with new sample
+        rmsThreshold.addSample(sv);
 
-	this.getSamplesSmooth = function() {
-		return samplesSmooth;
-	}
+        // speaking indicator is 1 if smoothened volume is above the threshold
+        var isSpeak = rmsThreshold.compareToThreshold(sv);
+
+        // add sample to array
+        samples.push({'vol': vol, 'sv' : sv, 'isSpeak' : isSpeak,'timestamp': timestamp});
+    };
+
+    // Returns true if the two samples are "close" enough together
+    this.isWithinTalkTimeout = function (curr, next) {
+        var diffInMs = next - curr;
+        return  diffInMs <= TALK_TIMEOUT;
+    };
+
+
+    // Returns true if talk interval is long enough
+    this.isMinTalkLength = function(talkInterval) {
+        var talkIntervalInMs = talkInterval.endTime - talkInterval.startTime;
+        return talkIntervalInMs >= MIN_TALK_LENGTH;
+    };
+
+    this.generateTalkIntervals = function() {
+        var talkIntervals = []; // startTime, endTime
+
+        var intervalStart = null;
+        var intervalEnd = null;
+
+        i = 0;
+        while (i < samples.length) {
+            j = i;
+
+            // traverse the array while samples belong to the same talking interval
+            while (j < samples.length - 1 && this.isWithinTalkTimeout(samples[j].timestamp, samples[j+1].timestamp))
+            {
+                j = j + 1
+            }
+
+            // new interval ended. Because each sample is sampleFreq millisecond long, we add this to the endTime
+            var newTalkInterval = {'startTime':samples[i].timestamp, 'endTime':samples[j].timestamp + this.sampleFreq};
+            if (!this.isMinTalkLength(newTalkInterval)) {
+                console.log("Not adding (too short): " + newTalkInterval.startTime + " " + newTalkInterval.endTime);
+            } else {
+                console.log("Adding: ",newTalkInterval.startTime,".",newTalkInterval.startTime.getMilliseconds()," ",newTalkInterval.endTime,".",newTalkInterval.endTime.getMilliseconds());
+                talkIntervals.push();
+            }
+
+            // set i to hold the next startTime
+            i = j + 1
+        }
+        return talkIntervals;
+    };
+
+
+    this.getSamples = function() {
+        return samples;
+    };
+
+    this.getSamplesSmooth = function() {
+        return samplesSmooth;
+    }
 }
 
 // Class for handling the threshold
 function RMSThreshold() {
-	var samples = []; // samples used for thresholding
-	var meanSquaredThreshold = 0; // (R)MS threshold. It's a sum of squared volumes
-	var meanSquaredThresholdCount = 0; // number of elements in the sum. required for calcualting the mean
+    var samples = []; // samples used for thresholding
+    var meanSquaredThreshold = 0; // (R)MS threshold. It's a sum of squared volumes
+    var meanSquaredThresholdCount = 0; // number of elements in the sum. required for calcualting the mean
 
-	this.addSample = function(v,timestamp) {
-		// Todo - remove obsolete samples
+    this.addSample = function(v,timestamp) {
+        // Todo - remove obsolete samples
 
-		// adding sample and updating the threshold
-		samples.push({'v': v, 'timestamp': timestamp});
-		meanSquaredThreshold = meanSquaredThreshold + (v*v);
-		meanSquaredThresholdCount++;
-	}
+        // adding sample and updating the threshold
+        samples.push({'v': v, 'timestamp': timestamp});
+        meanSquaredThreshold = meanSquaredThreshold + (v*v);
+        meanSquaredThresholdCount++;
+    };
 
-	this.compareToThreshold = function(v) {
-		return v*v > meanSquaredThreshold/meanSquaredThresholdCount ? 1 : 0;
-	}
+    this.compareToThreshold = function(v) {
+        return v*v > meanSquaredThreshold/meanSquaredThresholdCount ? 1 : 0;
+    }
+
 }
 
 // Class for smoothening the volume signal
 function SmoothArray() {
-	// Number of samples that will be used for smoothing the samples
-	var SAMPLES_SMOOTHING = 5;
-	var smoothArray = [];
-	var pos = 0;
+    // Number of samples that will be used for smoothing the samples
+    var SAMPLES_SMOOTHING = 5;
+    var smoothArray = [];
+    var pos = 0;
 
-	this.push = function(vol) {
-		// adds the sample to the correct location
-		console.log("Adding "+vol+" to array size "+smoothArray.length);
-		smoothArray[pos] = vol;
-      	pos = (pos +1) % SAMPLES_SMOOTHING;
-      	console.log("Pos is now "+pos+" to array size "+smoothArray.length);
+    this.push = function(vol) {
+        // adds the sample to the correct location
+        console.log("Adding "+vol+" to array size "+smoothArray.length);
+        smoothArray[pos] = vol;
+        pos = (pos +1) % SAMPLES_SMOOTHING;
+        console.log("Pos is now "+pos+" to array size "+smoothArray.length);
 
-      	// returns smoothened value
-      	var sum = smoothArray.reduce(function(a, b) { return a + b; });
-		var mean = sum / smoothArray.length;
-		return mean;
-	}
+        // returns smoothened value
+        var sum = smoothArray.reduce(function(a, b) { return a + b; });
+        var mean = sum / smoothArray.length;
+        return mean;
+    };
 
-	this.getSmoothArray = function() {
-		return smoothArray;
-	}
+    this.getSmoothArray = function() {
+        return smoothArray;
+    }
 }
 
-
 var myHeading = document.querySelector('h1');
-a = new DataAnalyzer();
+
+a = new DataAnalyzer(250);
 myHeading.textContent = 'Hello world!';
 a.addSample(1, new Date(2016,4,28,9,55,0,0));
-a.addSample(1, new Date(2016,4,28,9,55,0,200));
-a.addSample(1, new Date(2016,4,28,9,55,0,600));
-a.addSample(4, new Date(2016,4,28,9,55,0,600));
-a.addSample(4, new Date(2016,4,28,9,55,0,800));
-a.addSample(1, new Date(2016,4,28,9,55,1,000));
+a.addSample(1, new Date(2016,4,28,9,55,0,250));
+a.addSample(1, new Date(2016,4,28,9,55,0,500));
+a.addSample(4, new Date(2016,4,28,9,55,0,750));
+a.addSample(4, new Date(2016,5,28,9,55,1,000));
+a.addSample(1, new Date(2016,5,28,9,55,1,250));
 samples = a.getSamples();
+b = a.generateTalkIntervals();
 console.log();
 console.log("Done");
