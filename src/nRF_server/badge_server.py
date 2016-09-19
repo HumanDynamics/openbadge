1ceb8056de0bb5ddd79d915bd618668f129b9242
Abdulrahman Alotaibi <adminq80@gmail.com>
2016-09-19 10:50:10 -0400

    Changed the code that reads from device_macs.txt to allow for lowercase mac addresses

--- src/nRF_server/badge_server.py
+++ src/nRF_server/badge_server.py
@@ -34,7 +34,7 @@ ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 
 # create formatter and add it to the handlers
-#formatter = logging.Formatter('%(asctime)s - %(levelname)s - [%(mac)s] %(message)s')
+# formatter = logging.Formatter('%(asctime)s - %(levelname)s - [%(mac)s] %(message)s')
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
 ch.setFormatter(formatter)
@@ -42,6 +42,7 @@ ch.setFormatter(formatter)
 # add the handlers to the logger
 logger.addHandler(fh)
 logger.addHandler(ch)
+
 
 def get_devices(device_file="device_macs.txt"):
     """
@@ -54,21 +55,31 @@ def get_devices(device_file="device_macs.txt"):
         logger.error("Cannot find devices file: {}".format(device_file))
         exit(1)
     logger.info("Reading whitelist:")
-    devices = []
 
-    with open(device_file, 'r') as csvfile:
-        fil = filter(lambda row: row[0]!='#', csvfile)
-        fil = filter(lambda x: not re.match(r'^\s*$', x), fil)
-        rdr = csv.reader(fil, delimiter=b' ')
-        for row in rdr:
-            device = row[0]
-            devices.append(device)
+    regex = re.compile(r'^([A-Fa-f0-9]{2}(?::[A-Fa-f0-9]{2}){5}).*')
 
-        csvfile.close()
+    with open(device_file, 'r') as devices_macs:
+        devices = [regex.findall(line) for line in devices_macs]
+        devices = filter(lambda x: x, map(lambda x: x[0] if x else False, devices))
+        devices = [d.upper() for d in devices]
 
-    for device in devices:
-        logger.info("    {}".format(device))
+    for d in devices:
+        logger.info("    {}".format(d))
+
     return devices
+
+    # with open(device_file, 'r') as csvfile:
+    #     fil = filter(lambda row: row[0]!='#', csvfile)
+    #     fil = filter(lambda x: not re.match(r'^\s*$', x), fil)
+    #     rdr = csv.reader(fil, delimiter=b' ')
+    #     for row in rdr:
+    #         device = row[0]
+    #         devices.append(device)
+    #
+    #     csvfile.close()
+    #
+    #
+    # return devices
 
 
 def dialogue(bdg):
@@ -86,16 +97,16 @@ def dialogue(bdg):
             logger.info("saving chunks to file")
 
             # store in CSV file
-            fout = open(audio_file_name, "a")
-            for chunk in bdg.dlg.chunks:
-                ts_with_ms = "%0.3f" % chunk.ts
-                logger.info("CSV: Chunk timestamp: {}, Voltage: {}, Delay: {}, Samples in chunk: {}".format(ts_with_ms,chunk.voltage,chunk.sampleDelay,len(chunk.samples)))
-                fout.write("{},{},{},{}".format(addr,ts_with_ms,chunk.voltage,chunk.sampleDelay))
-                for sample in chunk.samples:
-                    fout.write(",{}".format(sample))
-                fout.write("\n")
-            fout.close()
-            logger.info("done writing")
+            with open(audio_file_name, "a") as fout:
+                for chunk in bdg.dlg.chunks:
+                    ts_with_ms = "%0.3f" % chunk.ts
+                    logger.info("CSV: Chunk timestamp: {}, Voltage: {}, Delay: {}, Samples in chunk: {}".format(
+                        ts_with_ms, chunk.voltage, chunk.sampleDelay, len(chunk.samples)))
+                    fout.write(bytes("{},{},{},{}".format(addr, ts_with_ms, chunk.voltage, chunk.sampleDelay)))
+                    for sample in chunk.samples:
+                        fout.write(b",{}".format(sample))
+                    fout.write(b"\n")
+                logger.info("done writing")
 
         else:
             logger.info("No mic data ready")
@@ -103,19 +114,18 @@ def dialogue(bdg):
         if bdg.dlg.scans:
             logger.info("Proximity scans received: {}".format(len(bdg.dlg.scans)))
             logger.info("saving proximity scans to file")
-            fout = open(proximity_file_name, "a")
-            for scan in bdg.dlg.scans:
-                ts_with_ms = "%0.3f" % scan.ts
-                logger.info("SCAN: scan timestamp: {}, voltage: {}, number: {}".format(
-                    ts_with_ms, scan.voltage, scan.numDevices))
-                if scan.devices:
-                    device_list = ''
-                    for dev in scan.devices:
-                        device_list += "[#{:x},{},{}]".format(dev.ID, dev.rssi, dev.count)
-                        fout.write(
-                            "{},{},{},{},{},{}\n".format(addr, scan.voltage, ts_with_ms, dev.ID, dev.rssi, dev.count))
-                    logger.info('  >  ' + device_list)
-            fout.close()
+            with open(proximity_file_name, "a") as fout:
+                for scan in bdg.dlg.scans:
+                    ts_with_ms = "%0.3f" % scan.ts
+                    logger.info("SCAN: scan timestamp: {}, voltage: {}, number: {}".format(
+                        ts_with_ms, scan.voltage, scan.numDevices))
+                    if scan.devices:
+                        device_list = ''
+                        for dev in scan.devices:
+                            device_list += "[#{:x},{},{}]".format(dev.ID, dev.rssi, dev.count)
+                            fout.write(bytes("{},{},{},{},{},{}\n".format(addr, scan.voltage, ts_with_ms, dev.ID,
+                                                                          dev.rssi, dev.count)))
+                        logger.info('  >  ' + device_list)
         else:
             logger.info("No proximity scans ready")
 
@@ -131,10 +141,10 @@ def scan_for_devices(devices_whitelist):
     scanned_devices = []
     for addr,device_info in all_devices.iteritems():
         if addr in devices_whitelist:
-            logger.debug("\033[1;7m\033[1;32mFound {}, added. Device info: {}\033[0m".format(addr,device_info))
+            logger.debug("\033[1;7m\033[1;32mFound {}, added. Device info: {}\033[0m".format(addr, device_info))
             scanned_devices.append({'mac':addr,'device_info':device_info})
         else:
-            logger.debug("Found {}, but not on whitelist. Device info: {}".format(addr,device_info))
+            logger.debug("Found {}, but not on whitelist. Device info: {}".format(addr, device_info))
     return scanned_devices
 
 
@@ -193,13 +203,13 @@ if __name__ == "__main__":
         logger.info("Done resetting BLE")
 
     if args.mode == "sync_device":
-        bdg = Badge(args.device,logger)
+        bdg = Badge(args.device, logger)
         bdg.sync_timestamp()
 
     if args.mode == "sync_all":
         whitelist_devices = get_devices()
         for addr in whitelist_devices:
-            bdg = Badge(addr,logger)
+            bdg = Badge(addr, logger)
             bdg.sync_timestamp()
             time.sleep(2)  # requires sleep between devices
 
@@ -212,15 +222,14 @@ if __name__ == "__main__":
             whitelist_devices = get_devices()
             logger.info("Scanning for devices...")
             scanned_devices = scan_for_devices(whitelist_devices)
-            fout = open(scans_file_name, "a")
-            for device in scanned_devices:
-                mac=device['mac']
-                scan_date=device['device_info']['scan_date']
-                rssi=device['device_info']['rssi']
-                voltage = device['device_info']['adv_payload']['voltage']
-                logger.debug("{},{},{:.2f},{:.2f}".format(scan_date, mac, rssi, voltage))
-                fout.write("{},{},{:.2f},{:.2f}\n".format(scan_date, mac, rssi, voltage))
-            fout.close()
+            with open(scans_file_name, "a") as fout:
+                for device in scanned_devices:
+                    mac = device['mac']
+                    scan_date=device['device_info']['scan_date']
+                    rssi=device['device_info']['rssi']
+                    voltage = device['device_info']['adv_payload']['voltage']
+                    logger.debug("{},{},{:.2f},{:.2f}".format(scan_date, mac, rssi, voltage))
+                    fout.write("{},{},{:.2f},{:.2f}\n".format(scan_date, mac, rssi, voltage))
             time.sleep(5)  # give time to Ctrl-C
 
     # pull data from all devices
@@ -233,7 +242,7 @@ if __name__ == "__main__":
         init_proximity_ts = now_ts
         init_audio_ts, init_audio_ts_fract = now_ts, now_ts_fract
 
-        badges = {} # Keeps a list of badge objects
+        badges = {}  # Keeps a list of badge objects
         while True:
             logger.info("Scanning for devices...")
             whitelist_devices = get_devices()
@@ -266,4 +275,4 @@ if __name__ == "__main__":
             time.sleep(2)  # requires sleep between devices
         time.sleep(5)  # allow BLE time to disconnect
 
-exit(0)
+    exit(0)
