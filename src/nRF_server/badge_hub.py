613b17d21478b0b2d5263375b7b30f4dd83e0177
Abdulrahman Alotaibi <adminq80@gmail.com>
2016-09-22 21:40:12 -0400

    Printing exception to STDout

--- src/nRF_server/badge_hub.py
+++ src/nRF_server/badge_hub.py
@@ -139,13 +139,13 @@ def dialogue(bdg):
             bdg.last_audio_ts = last_chunk.ts
             bdg.last_audio_ts_fract = last_chunk.fract
             bdg.last_proximity_ts = last_scan.ts
-            requests.put(BADGE(bdg.key), data={
+            requests.patch(BADGE(bdg.key), data={
                 'last_audio_ts': bdg.last_audio_ts,
                 'last_audio_ts_fract': bdg.last_audio_ts_fract,
                 'last_proximity_ts': bdg.last_proximity_ts
             })
-        except Exception():
-            pass
+        except Exception() as e:
+            print(e)
 
 
 def scan_for_devices(devices_whitelist):
