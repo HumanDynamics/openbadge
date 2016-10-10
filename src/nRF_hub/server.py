b410b9276de9ecb46e4e7ededc7bb89b9d069636
Oren Lederman <oren.lederman@gmail.com>
2016-10-10 19:14:33 -0400

    added a utility that registers the hub's IP (based on the hostname)

--- src/nRF_hub/server.py
+++ src/nRF_hub/server.py
@@ -4,7 +4,7 @@ import settings
 
 SERVER = 'http://'+settings.BADGE_SERVER_ADDR+':'+settings.BADGE_SERVER_PORT+'/'
 BADGES_ENDPOINT = '{}badges/'.format(SERVER)
-
+HUBS_ENDPOINT = '{}hubs/'.format(SERVER)
 
 def _badge(x):
     """
@@ -14,4 +14,15 @@ def _badge(x):
     """
     return '{}{}/'.format(BADGES_ENDPOINT, x)
 
+
+def _hub(x):
+    """
+    Generates endpoint for a given hub
+    :param x: hostname of the hub
+    :return:
+    """
+    return '{}{}/'.format(HUBS_ENDPOINT, x)
+
+
 BADGE_ENDPOINT = _badge
+HUB_ENDPOINT = _hub
