8fcb75278ef0f7afc4b2ddec46989ca68724eb91
Andrew Bartow <abartow@mit.edu>
2016-10-31 19:00:18 -0400

    Update debug log test

--- IntegrationTests/test_1_debug_log.py
+++ IntegrationTests/test_1_debug_log.py
@@ -1,7 +1,7 @@
 from integration_test import *
 
 class DebugLogTestCase(IntegrationTest):
-	def testCase(self, badge):
+	def testCase(self, badge, logger):
 		debug_log = badge.get_debug_log()
 		self.assertEqual(debug_log, "Andrew Rulez!\x00")
 
