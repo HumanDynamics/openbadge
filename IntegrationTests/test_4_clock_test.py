aacc811376cc8eff527987a1fa6bf2764b3e51f9
Andrew Bartow <abartow@mit.edu>
2016-10-31 18:45:36 -0400

    More clenaup changes

--- IntegrationTests/test_4_clock_test.py
+++ IntegrationTests/test_4_clock_test.py
@@ -2,19 +2,19 @@ import time
 from integration_test import *
 
 class ClockTestCase(IntegrationTest):
-	def testCase(self, badge):
+	def testCase(self, badge, logger):
 		# Sync current time.
 		badge.get_status()
 
 		time.sleep(.25)
 
 		start_time = badge.get_status().timestamp_seconds
-		print "Start time:", start_time
+		logger.info("Start time: %f", start_time)
 
 		time.sleep(5)
 
 		end_time = badge.get_status().timestamp_seconds
-		print "End time:", end_time
+		logger.info("End time: %f", end_time)
 
 		self.assertAlmostEqual(end_time, start_time + 5, delta=1)
 
