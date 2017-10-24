49f90eba163c00ec04901db17fa0589538dffc52
Oren Lederman <oren.lederman@gmail.com>
2017-10-24 15:39:30 -0400

    Minor changes. Generated centroids and bom

--- eagle/nRF_badge/Badge_03_v5/move.sh
+++ eagle/nRF_badge/Badge_03_v5/move.sh
@@ -1,5 +1,5 @@
-mv badge_03v5.bottom* Badge_03v5_RTC_export_20160906
-mv badge_03v5.top* Badge_03v5_RTC_export_20160906
-mv badge_03v5.drill* Badge_03v5_RTC_export_20160906
-mv badge_03v5.outline* Badge_03v5_RTC_export_20160906
+mv badge_03v5.bottom* Badge_03v5_export_20171024
+mv badge_03v5.top* Badge_03v5_export_20171024
+mv badge_03v5.drill* Badge_03v5_export_20171024
+mv badge_03v5.outline* Badge_03v5_export_20171024
 
