a742618d236450fa49b8c10b0831124a1f884c48
Oren Lederman <oren.lederman@gmail.com>
2017-10-23 21:00:56 -0400

    Created new eagle version. Added BAT variant with no cream in center. Replaced top part to new variant

---
+++ eagle/nRF_badge/Badge_03_v5/move.sh
@@ -0,0 +1,5 @@
+mv badge_03v5.bottom* Badge_03v5_RTC_export_20160906
+mv badge_03v5.top* Badge_03v5_RTC_export_20160906
+mv badge_03v5.drill* Badge_03v5_RTC_export_20160906
+mv badge_03v5.outline* Badge_03v5_RTC_export_20160906
+
