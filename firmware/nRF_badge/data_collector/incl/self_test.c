d2ffa625f000216540bcd8cdea28cf9732452b25
Oren Lederman <oren.lederman@gmail.com>
2017-12-03 14:15:37 -0500

    Very basic tester code to check the LIS2DH whoami register

--- firmware/nRF_badge/data_collector/incl/self_test.c
+++ firmware/nRF_badge/data_collector/incl/self_test.c
@@ -66,6 +66,92 @@ bool testMicAboveThreshold() {
     return lastSample > avg_with_sd;
 }
 
+//TODO: move accelerometer code somewhere else
+#define SPIM0_SS_PIN_2   2 // accelerometer
+#define LIS2DH_WHO_AM_I_REGISTER 		0x0F
+#define LIS2DH_WHO_AM_I_VALUE           0x33
+volatile bool transmission_completed_spi = false;
+
+void accel_spi_evt_handler(spi_master_evt_t spi_master_evt)
+{
+    switch(spi_master_evt.evt_type){
+		case SPI_MASTER_EVT_TRANSFER_COMPLETED:
+			//Transmission done.
+			transmission_completed_spi = true;
+			//rx_buffer must have received data now.
+	    	break;
+		default:
+			// no implementation is required for now.
+		    break;
+	}
+}
+
+void accel_spi_init()
+{
+    uint32_t err_code = NRF_SUCCESS;
+
+    //manually handle SS pin
+    nrf_gpio_pin_set(SPIM0_SS_PIN_2);
+    nrf_gpio_cfg_output(SPIM0_SS_PIN_2);
+
+    // Configure SPI master.
+    spi_master_config_t spi_config = {
+        SPI_FREQUENCY_FREQUENCY_M8, // Serial clock frequency 8 Mbps.
+        SPIM0_SCK_PIN,              // Defined in badge_03.h
+        SPIM0_MISO_PIN,
+        SPIM0_MOSI_PIN,
+        SPIM0_SS_PIN_2,                         //    // SS, we'll handle that manually
+        APP_IRQ_PRIORITY_LOW,       // Interrupt priority LOW.
+        SPI_CONFIG_ORDER_MsbFirst,  // Bits order ?
+        SPI_CONFIG_CPOL_ActiveLow, // Serial clock polarity ACTIVELOW.
+        SPI_CONFIG_CPHA_Trailing,    // Serial clock phase TRAILING.
+        0                           // Don't disable all IRQs.
+    };
+
+    err_code = spi_master_open(SPI_MASTER_0, &spi_config);
+    if (err_code != NRF_SUCCESS)  {
+        nrf_gpio_pin_write(LED_2,LED_ON);  //turn on LED
+        debug_log("Err\r\n");
+    }
+
+    // Register event handler for SPI master.
+    spi_master_evt_handler_reg(SPI_MASTER_0, accel_spi_evt_handler);
+}
+
+
+
+void accel_test()
+{
+    debug_log("Init accelerometer\r\n");
+    accel_spi_init();
+
+    debug_log("Asking whoami\r\n");
+    transmission_completed_spi = false;
+
+    uint8_t txBuf[1] = {LIS2DH_WHO_AM_I_REGISTER | 0x80};      //call whoami
+    uint8_t rxBuf[2] = {0,0};
+    uint32_t err_code = spi_master_send_recv(SPI_MASTER_0,txBuf,sizeof(txBuf),rxBuf,2);
+	if (err_code != NRF_SUCCESS)
+	{
+	    //Data transmission failed.
+	    nrf_gpio_pin_write(LED_2,LED_ON);  //turn on LED
+        debug_log("Err\r\n");
+	}
+
+    //while(!transmission_completed_spi);//wait for the data to transfer complete.
+    while(spi_busy());
+    uint8_t whoami = rxBuf[1];
+    if (whoami == LIS2DH_WHO_AM_I_VALUE)
+    {
+        debug_log("LIS2DH found!\r\n");
+    }
+    else
+    {
+        debug_log("LIS2DH not found!\r\n");
+        nrf_gpio_pin_write(LED_2,LED_ON);  //turn on LED
+    }
+}
+
 void runSelfTests()
 {
     debug_log("Running self-tests.\r\n");
