6c63eddcd95ec88f3c2673b2efc0f8b0866150f9
Zachary Pitcher <zackpi@mit.edu>
2018-03-29 16:22:21 -0400

    Some cleanup

--- compose/entry_point.sh
+++ compose/entry_point.sh
@@ -3,6 +3,9 @@ set -e
 if [ "$1" = 'make' ]; then
     cd /app/firmware/nRF_badge/data_collector
     exec "$@"
+elif [ "$1" = 'rssi_test' ]; then
+    cd /app/firmware/nRF_badge/rssi_tester
+    exec "make" ${@:2} 
 elif [ "$1" = 'getMAC' ]; then
     cd /app/firmware/nRF_badge/util
     ./getMAC.sh
