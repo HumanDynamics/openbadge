e47c8439a15606ac1ee58631b3e70ad19d27561e
Oren Lederman <oren.lederman@gmail.com>
2018-05-11 15:19:07 -0400

    Some cleanup

--- compose/entry_point.sh
+++ compose/entry_point.sh
@@ -2,13 +2,7 @@
 set -e
 if [ "$1" = 'make' ]; then
     cd /app/firmware/nRF_badge/data_collector
-    if [ "$2" = 'test' ]; then
-        make -f Maketests "${@:3}"
-        #./_test/run_all_tests.o
-        #Srm -rf _test
-    else
-        exec "$@"
-    fi
+    exec "$@"
 elif [ "$1" = 'make_rssi_scanner' ]; then
     cd /app/firmware/nRF_badge/rssi_scanner
     exec "make" ${@:2} 
