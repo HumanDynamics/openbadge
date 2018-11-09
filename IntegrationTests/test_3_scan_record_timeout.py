c4f54f01efde2e6c2225b1823455b981ba19a465
Jordan Reedie <jjreedie@gmail.com>
2018-11-08 21:21:22 -0500

    Update test 3 with new protocol
     added tests for battery/accelerometer

--- IntegrationTests/test_3_scan_record_timeout.py
+++ IntegrationTests/test_3_scan_record_timeout.py
@@ -3,23 +3,32 @@ import time
 from integration_test import *
 
 class ScanRecordTimeoutTestCase(IntegrationTest):
+
 	def testCase(self, badge, logger):
-		badge.start_recording(timeout_minutes=1)
-		badge.start_scanning(timeout_minutes=1)
+		badge.start_microphone(timeout_minutes=1)
+		badge.start_scan(timeout_minutes=1)
+		badge.start_accelerometer(timeout_minutes=1)
+		badge.start_battery(timeout_minutes=1)
 
 		status = badge.get_status()
-		self.assertTrue(status.collector_status)
-		self.assertTrue(status.scanner_status)
+		self.assertTrue(status.microphone_status)
+		self.assertTrue(status.scan_status)
+		self.assertTrue(status.accelerometer_status)
+		self.assertTrue(status.battery_status)
 
 		time.sleep(59)
 		status = badge.get_status()
-		self.assertTrue(status.collector_status)
-		self.assertTrue(status.scanner_status)
+		self.assertTrue(status.microphone_status)
+		self.assertTrue(status.scan_status)
+		self.assertTrue(status.accelerometer_status)
+		self.assertTrue(status.battery_status)
 
 		time.sleep(121)
 		status = badge.get_status()
-		self.assertFalse(status.collector_status)
-		self.assertFalse(status.scanner_status)
+		self.assertFalse(status.microphone_status)
+		self.assertFalse(status.scan_status)
+		self.assertFalse(status.accelerometer_status)
+		self.assertFalse(status.battery_status)
 
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
